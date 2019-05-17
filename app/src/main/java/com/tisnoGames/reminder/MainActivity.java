be8e18a567a99ee626138f200273261f7f66012a
Thijs Notkamp <t.notkamp@gmail.com>
2019-05-17 20:36:56 +0200

    0.0.2 Created Animation + Scroll Layout

--- app/src/main/java/com/tisnoGames/reminder/MainActivity.java
+++ app/src/main/java/com/tisnoGames/reminder/MainActivity.java
@@ -1,6 +1,4 @@
 package com.tisnoGames.reminder;
-
-import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -10,17 +8,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     private TextView mTextMessage;
     private CardView mainCard;
     private ConstraintLayout layout;
     private ArrayList<CardView> allCards = new ArrayList<>();
+    private int layoutHeight = 40;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -51,20 +48,41 @@ public class MainActivity extends AppCompatActivity {
         mainCard = (CardView) findViewById(R.id.card_view);
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        layout = (ConstraintLayout) findViewById(R.id.container);
+        layout = (ConstraintLayout) findViewById(R.id.ScrollLayout);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+
+    //Create a new card with a reminder on it.
     public void newCard(View view){
-        System.out.println("test");
+
+
         CardView temp = new CardView(this);
-        temp.setCardBackgroundColor(Color.BLUE);
+        temp.setCardBackgroundColor(Color.WHITE);
         temp.setMinimumWidth(1020);
         temp.setMinimumHeight(300);
         temp.setCardElevation(10);
-        temp.setY(300f);
+        temp.setY(50f);
         temp.setX(30);
         layout.addView(temp);
+        moveCards();
+        allCards.add(temp);
+
+        //Add height to the navigation so that the user cant scroll lower than the cards, but all the cards are still visible
+        layoutHeight += temp.getMinimumHeight()+40;
+        layout.setMinHeight(layoutHeight);
     }
+
+    //Move the cards down so the new card is above everything else.
+    private void moveCards(){
+        //Make every card in the arrayList move
+        for (final CardView temp : allCards)
+        {
+            temp.animate().y(temp.getY()+temp.getHeight()+40).setDuration(500);
+        }
+
+
+    }
+
 
 }
