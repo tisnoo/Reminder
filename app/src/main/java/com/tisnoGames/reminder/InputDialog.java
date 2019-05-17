91ddcd93caa96d7017b226b43a231abb518237ab
Thijs Notkamp <t.notkamp@gmail.com>
2019-05-17 21:55:16 +0200

    0.0.3 Test String and Dialog

---
+++ app/src/main/java/com/tisnoGames/reminder/InputDialog.java
@@ -0,0 +1,28 @@
+package com.tisnoGames.reminder;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+public class InputDialog extends DialogFragment {
+
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("fire missile")
+                .setPositiveButton("Go", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                    }
+                })
+                .setNegativeButton("close", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
--- app/src/main/java/com/tisnoGames/reminder/MainActivity.java
+++ app/src/main/java/com/tisnoGames/reminder/MainActivity.java
@@ -4,20 +4,22 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.WrapperListAdapter;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity  {
 
     private TextView mTextMessage;
-    private CardView mainCard;
     private ConstraintLayout layout;
     private ArrayList<CardView> allCards = new ArrayList<>();
-    private int layoutHeight = 40;
+    private int layoutHeight = 30;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -40,12 +42,12 @@ public class MainActivity extends AppCompatActivity  {
     };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
-        mainCard = (CardView) findViewById(R.id.card_view);
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         layout = (ConstraintLayout) findViewById(R.id.ScrollLayout);
@@ -54,18 +56,44 @@ public class MainActivity extends AppCompatActivity  {
 
 
     //Create a new card with a reminder on it.
-    public void newCard(View view){
+    public void newCard(View view)
+    {
+
+        //Create popup known as "InputDialog"
+        FragmentManager fm = getSupportFragmentManager();
+        InputDialog id = new InputDialog();
+        id.showNow(fm,"go");
+
+        //Setting up the text to be inserted into the card
+        TextView cardText = new TextView(this);
+        cardText.setText("\"Lorem ipsum dolor sit amet, consect d officia deserunt mollit anim id est laborum l;fsjdalkj ;lkjsfd;alkj flkjdsflkj dslkjfdsf s fdsdf sfd ljsdf slfd jdsfnsdfn jfsldf jls fdlsjdf jsfdj.\"");
+        cardText.setY(30);
+        cardText.setX(30);
 
 
+        //Setting properties for newly made card
         CardView temp = new CardView(this);
         temp.setCardBackgroundColor(Color.WHITE);
         temp.setMinimumWidth(1020);
+        cardText.setWidth(temp.getMinimumWidth()-30);
+
+        //Adding the text to the card.
+        temp.addView(cardText);
+        cardText.getLineCount();
+        temp.setCardElevation(10f);
+        temp.setY(10f);
+        temp.setX(30f);
         temp.setMinimumHeight(300);
-        temp.setCardElevation(10);
-        temp.setY(50f);
-        temp.setX(30);
+        temp.setAlpha(0f);
+
+
+        //Add Card to The Layout (inside of the scrollView)
         layout.addView(temp);
-        moveCards();
+
+        //Move the cards before adding the newly made card to the cardArraylist so that the card doesnt move down
+        moveCards(temp);
+
+        //Add the card to the card Arraylist
         allCards.add(temp);
 
         //Add height to the navigation so that the user cant scroll lower than the cards, but all the cards are still visible
@@ -74,15 +102,13 @@ public class MainActivity extends AppCompatActivity  {
     }
 
     //Move the cards down so the new card is above everything else.
-    private void moveCards(){
+    private void moveCards(CardView newCard)
+    {
         //Make every card in the arrayList move
-        for (final CardView temp : allCards)
+        for (CardView temp : allCards)
         {
             temp.animate().y(temp.getY()+temp.getHeight()+40).setDuration(500);
         }
-
-
+        newCard.animate().alpha(1).setDuration(800);
     }
-
-
 }
